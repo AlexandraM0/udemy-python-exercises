db89cbb099bc46d992ec9517a95a6ff256341244
AlexandraM0 <alexandra.moghina97@gmail.com>
2024-02-02 15:26:28 +0200

    Variables, operators and strings

---
+++ variables-operators-strings.py
@@ -0,0 +1,53 @@
+# 1. Write a script that adds 15 and 30 and divides the sum by 2. Print out the answer.
+x = 15
+y = 30
+sum = x + y
+print("The sum is:", sum)
+print("The final result is:", sum/2)
+
+# 2. Initialize two variables and use arithmetic operators to add, subtract, multiply, divide, and find the remainder.
+num1 = int(input ("The first number: "))
+num2 = int(input ("The second number: "))
+sum = num1 + num2
+sub = num1 + num2
+multiply = num1 * num2
+division = num1 / num2
+mod = num1 % num2
+
+print ("Sum is: ", sum)
+print ("Substraction is: ", sub)
+print ("Multiply is: ", multiply)
+print ("Division is: " , division)
+print ("Modulo is: ", mod)
+
+# 3. Create a variable called name and store your name in it as a string.
+name = "Anda"
+print(name)
+
+# 4. Create three variables in one line and assign each one a different food item.
+a, b, c = "banana", "apple", "strawberry"
+print(a, b, c)
+
+# 5. Print out "Hello" ten times using arithmetic operators.
+print ("Hello \n" * 10)
+
+# 6. Set your name and age variables in one line with multiple assignment
+name, age = "Anda", 26
+print (name,age)
+
+# 7. Create two strings and then create a third variable combining both of the two original strings.
+string1 = "The sky is blue"
+string2 = "the sun is shinning bright"
+print (string1 + " and " + string2)
+
+# 8. Create a string and print the fourth letter of the word.
+sentence = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+print (sentence[3])
+
+# 9. Create a string and print the letters from index 0 to 5.
+sentence1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+print (sentence1[0:6])
+
+# 10. Create a variable and print all the letters from the index 1 until the end.
+sentence2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+print (sentence2[1:])
